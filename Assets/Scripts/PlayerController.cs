2ed75e8e51c5347fc3b90086b20dae0ffd6a2bc7
Macciejj <macios99@gmail.com>
2022-07-02 22:48:27 +0200

    Refactoring 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float speed = 1f;
+
     private PlayerInputActions inputAction;
     private InputAction move;
     private Vector2 direction = Vector2.zero;
-    [SerializeField] float speed = 1f;
-
+    
     private void Awake()
     {
         move = new PlayerInputActions().playercontrol.movement;
     }
+
     private void OnEnable()
     {       
         move.Enable();
-    }
-    private void OnDisable()
-    {
-        move.Disable();
     }
 
     private void FixedUpdate()
@@ -28,14 +26,21 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
-    private void Move()
-    {
-        transform.Translate(direction.x * speed, 0, direction.y * speed);
-    }
-
     private void Update()
     {
         direction = move.ReadValue<Vector2>();
         direction.Normalize();
     }
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction.x * speed, 0, direction.y * speed);
+    }
+
+
 }
