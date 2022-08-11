dc93948de268799c6080efe6b3b73d926d22c3c9
Macciejj <macios99@gmail.com>
2022-08-11 17:52:56 +0200

    pool delete

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -5,26 +5,13 @@ using UnityEngine;
 public class WaveManager : MonoBehaviour
 {
     [SerializeField] GameObject[] wavePrefabs;
-    [SerializeField] List<GameObject> waves = new List<GameObject>();
     private UICountdown uICountdown;
 
     private void Awake()
     {
-        foreach (var item in wavePrefabs)
-        {
-            waves.Add(Instantiate(item, transform.position, Quaternion.identity));
-        }
-        foreach (var item in waves)
-        {
-            item.SetActive(false);
-        }
-        
-    }
-
-    private void Start()
-    {
         uICountdown = FindObjectOfType<UICountdown>();
     }
+
 
     private void Update()
     {       
@@ -36,7 +23,7 @@ public class WaveManager : MonoBehaviour
         if (IsOnlyPlayerOnBoard())
         {
             int randomNumber = Random.Range(0, wavePrefabs.Length);
-            waves[randomNumber].SetActive(true);           
+            Instantiate(wavePrefabs[randomNumber], transform);        
         }
     }
 
